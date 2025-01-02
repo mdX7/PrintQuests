1f12e6f699abf479ac3837297eda3d60e6f52e48
ModoX <moardox@gmail.com>
2025-01-02 07:26:24 +0100

    Added some kind of categories

--- TrackingQuests.lua
+++ TrackingQuests.lua
@@ -1,8 +1,11 @@
 
 PrintQuests.UnsurelyNamedTrackingQuests = {
     [83281] = "Weaver Weekly turnin???",
-    [86931] = "Weaver Weekly turnin???",
-    [84538] = "Weaver Accessory rank / Weaver weekly turnin?",
+    [86931] = "Weaver Weekly turnin / Bronze Celebration Tokens on first weekly turnin?",
+    [81602] = "Severed Threads - The General: Reached Crony (2/8) reputation",
+    [81626] = "Severed Threads - The General: Reached Crony (2/8) reputation",
+    [81598] = "Severed Threads - The Weaver: Reached Crony (2/8) reputation",
+    [81627] = "Severed Threads - The Weaver: Reached Crony (2/8) reputation",
     [80415] = "War Supply Chest on Siren Isle/Azj'kahet looted?",
     [80416] = "War Supply Chest on Siren Isle/Azj'kahet looted?",
     [84873] = "Siren Isle Rare: Kill X Rares?",
@@ -10,35 +13,22 @@ PrintQuests.UnsurelyNamedTrackingQuests = {
     [86171] = "Siren Isle: Rune-Sealed Coffer looted (in Stormtide Dregs)?",
     [85708] = "Siren Isle: Special Assignment: Storm's a Brewin unlocked?",
     [86486] = "Hungry, Hungry Snapdragon 2/X?",
-    [85723] = "Anniversary Worldboss: First Worldboss kill of day/week? Azuregos",
+    [85723] = "Anniversary Worldboss: First Worldboss kill of day/week? Azuregos/Doomwalker",
     [85168] = "Anniversary Worldboss: Second Worldboss kill of day/week? Lord Kazzak",
+    [84614] = "Khaz Algar Worldboss killed? Aggregation of Horrors",
 }
 
 PrintQuests.ConfidentlyNamedTrackingQuests = {
+    -----------------------------------------------------------------------------------------------------------------------
+    -- Exiles Reach
     [58883] = "Stitched Leather Boots looted",
-    [84801] = "Siren Isle Rare: Ghostmaker killed daily",
-    [84792] = "Siren Isle Rare: Ikir the Flotsurge killed daily",
-    [85403] = "Siren Isle Rare: Tempest Talon killed daily",
-    [84798] = "Siren Isle Rare: Slaughtershell killed daily",
-    [85405] = "Siren Isle Rare: Zek'hul the shipbreaker killed daily",
-    [85404] = "Siren Isle Rare: Brinebough killed daily",
-    [84805] = "Siren Isle Rare: Asbjorn the Bloodsoaked killed daily",
-    [84794] = "Siren Isle Rare: Wreckwater killed daily",
-    [85406] = "Siren Isle Rare: Ksvir the Forgotten killed daily",
-    [86933] = "Siren Isle Rare: Snacker killed daily",
-    [84796] = "Siren Isle Rare: Grimmgull killed daily",
-    [84797] = "Siren Isle Rare: Gunnlord the Sea-Drinker killed daily",
-    [84803] = "Siren Isle Rare: Siris the Sea Scorpion killed daily",
-    [84802] = "Siren Isle Rare: Coralweaver Calliso killed daily",
-    [84795] = "Siren Isle Rare: Bloodbrine killed daily",
-    [86779] = "Siren Isle Rare: Shardsong killed daily",
+    
+    -----------------------------------------------------------------------------------------------------------------------
+    -- Dornogal
     [84799] = "Siren Isle Rare: Plank-Master Bluebelly daily",
     [84800] = "Siren Isle Rare: Chef Chum Platter daily",
     [85156] = "Angorla stay a while and listen",
     [85103] = "Apprentice Tanmar stay a while and listen",
-    [85669] = "Siren Isle Excavation: Gravesludge killed in The Drain",
-    [86732] = "Siren Isle Chest: Stone Carvers Scramseax looted",
-    [84101] = "Brann reached Level 40",
     [84009] = "Alleria Stay a while and listen after The Fleet Arrives",
     [84345] = "Turalyon Stay a while and listen after Embassies and Envoys",
     [82461] = "Dagran Thaurissan II Stay a while and listen in The Archive",
@@ -46,23 +36,72 @@ PrintQuests.ConfidentlyNamedTrackingQuests = {
     [85682] = "Magni Bronzebeard Stay a while and listen in front of The Archive",
     [82542] = "Rooktender Lufsela Stay a while and listen in front of Thrall, Shraubendre",
     [84813] = "Rooktender Lufsela Stay a while and listen in Dhar Oztan",
-    [84539] = "Renown 22 Severed Threads",
-    [84631] = "Awakening The Machine Wave 5 cleared",
-    [84632] = "Awakening The Machine Wave 10 cleared",
-    [84633] = "Awakening The Machine Wave 15 cleared",
-    [84634] = "Awakening The Machine Wave 20 cleared",
+    
+    -- -----------------------------------------------------------------------------------------------------------------------
+    -- Delves
+    [84101] = "Brann reached Level 40",
+    
+    -- -----------------------------------------------------------------------------------------------------------------------
+    -- Severed Threads
+    [84539] = "Severed Threads: Renown 22",
+    -- The Vizier
+    [81605] = "Severed Threads - The Vizier: Reached Acquiantance (1/8) reputation",
+    [81606] = "Severed Threads - The Vizier: Reached Crony (2/8) reputation",
+    [81607] = "Severed Threads - The Vizier: Reached Accomplice (3/8) reputation",
+    -- missing Collaborator 4/8
+    [84534] = "Severed Threads - The Vizier: Reached Accessory (5/8) reputation",
+    [84535] = "Severed Threads - The Vizier: Reached Abettor (6/8) reputation",
+    [84536] = "Severed Threads - The Vizier: Reached Conspirator (7/8) reputation",
+    [84537] = "Severed Threads - The Vizier: Reached Mastermind (8/8) reputation",
+    -- The Weaver
+    [81597] = "Severed Threads - The Weaver: Reached Acquiantance (1/8) reputation",
+    -- Crony 2/8 has 2 tracking quests, ref UnsurelyNamedTrackingQuests
+    [81599] = "Severed Threads - The Weaver: Reached Accomplice (3/8) reputation",
+    [81600] = "Severed Threads - The Weaver: Reached Collaborator (4/8) reputation",
+    [84538] = "Severed Threads - The Weaver: Reached Accessory (5/8) reputation",
+    -- The General
+    [81601] = "Severed Threads - The General: Reached Acquiantance (1/8) reputation",
+    -- Crony 2/8 has 2 tracking quests, ref UnsurelyNamedTrackingQuests
+    [81603] = "Severed Threads - The General: Reached Accomplice (3/8) reputation",
+    [81604] = "Severed Threads - The General: Reached Collaborator (4/8) reputation",
+    [84530] = "Severed Threads - The General: Reached Accessory (5/8) reputation",
+    [80545] = "Severed Threads: Weekly Pact selected: The General",
+    [80544] = "Severed Threads: Weekly Pact selected: The Weaver",
+    [80546] = "Severed Threads: Weekly Pact selected: The Vizier",
+    
+    -- -----------------------------------------------------------------------------------------------------------------------
+    -- Awakening The Machine
+    [84631] = "Awakening The Machine: Wave 5/20 cleared",
+    [84632] = "Awakening The Machine: Wave 10/20 cleared",
+    [84633] = "Awakening The Machine: Wave 15/20 cleared",
+    [84634] = "Awakening The Machine: Wave 20/20 cleared",
+    [84644] = "Awakening The Machine: Awakened Cache looted (right)",
+    [84642] = "Awakening The Machine: Awakened Cache looted (front left)",
+    [84646] = "Awakening The Machine: Awakened Cache looted (back left)",
+    [84647] = "Awakening The Machine: Awakened Cache looted (back center)",
+    
+    -- -----------------------------------------------------------------------------------------------------------------------
+    -- Mining
     [83054] = "Mining: Slab of Slate 1/5 looted",
     [83053] = "Mining: Slab of Slate 2/5 looted",
     [83052] = "Mining: Slab of Slate 3/5 looted",
     [83051] = "Mining: Slab of Slate 4/5 looted",
     [83050] = "Mining: Slab of Slate 5/5 looted",
     [83049] = "Mining: Erosion-Polished Slate looted",
-    [81881] = "Hallowfall Rare: Funglour killed daily",
-    [84229] = "Faded Engineer Scriblings used",
     [81391] = "Exceptional Miner's Notes used",
-    [83063] = "Machine-Learned Engineering Notes used",
     [83062] = "Machine-Learned Mining Notes used",
-    [81355] = "Convincingly Realistic Jumper Cables First Craft done",
+    [83733] = "Algari Treatise on Mining used",
+    
+    -- -----------------------------------------------------------------------------------------------------------------------
+    -- Engineering
+    [84229] = "Faded Engineer Scriblings used",
+    [83063] = "Machine-Learned Engineering Notes used",
+    [83728] = "Algari Treatise on Engineering used",
+    [81355] = "Engineering First Time Craft: Convincingly Realistic Jumper Cables",
+    [81356] = "Engineering First Time Craft: Algari Repair Bot 11O",
+    
+    -- -----------------------------------------------------------------------------------------------------------------------
+    -- 20th Anniversary
     [86282] = "20th Anniversary Blackrock Depths Miniboss: Pyromancer Loregrain killed LFR",
     [86285] = "20th Anniversary Blackrock Depths Miniboss: Pyromancer Loregrain killed Normal",
     [86278] = "20th Anniversary Blackrock Depths Miniboss: High Interrogator Gerstahn killed LFR",
@@ -78,9 +117,15 @@ PrintQuests.ConfidentlyNamedTrackingQuests = {
     [86286] = "20th Anniversary Blackrock Depths Miniboss: Phalanx killed Normal",
     [86293] = "20th Anniversary Blackrock Depths Miniboss: Phalanx killed Heroic",
     [84665] = "20th Anniversary: The Originals / Timely Gate Crashers turnin",
+    [60214] = "Anniversary Worldboss: Doomwalker Kill",
+    [84282] = "Anniversary Worldboss: Sha of Anger Kill",
+    [84256] = "Anniversary Worldboss: Archavon the Stone Watcher Kill",
     [47462] = "Anniversary Worldboss: Azuregos Kill",
     [47461] = "Anniversary Worldboss: Lord Kazzak Kill",
     [47463] = "Anniversary Worldboss: Dragon of Nightmare Kill",
+    
+    -- -----------------------------------------------------------------------------------------------------------------------
+    -- Isle of Dorn
     [79685] = "Isle of Dorn Rare: Sandres the Relicbearer killed daily",
     [81892] = "Isle of Dorn Rare: Springbubble killed daily",
     [81893] = "Isle of Dorn Rare: Bloodmaw killed daily",
@@ -113,12 +158,48 @@ PrintQuests.ConfidentlyNamedTrackingQuests = {
     [81904] = "Isle of Dorn Rare: Twice-Stinger the Wretched killed daily",
     [84033] = "Isle of Dorn Rare: Twice-Stinger the Wretched killed weekly reputation",
     [85159] = "Isle of Dorn Rare: Zovex killed weekly reputation",
+    
+    -- -----------------------------------------------------------------------------------------------------------------------
+    -- Ringing Deeps
     [80003] = "Ringing Deeps Rare: Disturbed Earthgorger killed daily",
     [84050] = "Ringing Deeps Rare: Disturbed Earthgorger killed weekly reputation",
     [81562] = "Ringing Deeps Rare: Charmonger Rare killed daily",
     [84044] = "Ringing Deeps Rare: Charmonger Rare killed weekly reputation",
     [81511] = "Ringing Deeps Rare: Coalesced Monstrosity Rare killed daily",
     [84045] = "Ringing Deeps Rare: Coalesced Monstrosity Rare killed weekly reputation",
-    [39751] = "Classic Timewalking LFG completion",
+    
+    -- -----------------------------------------------------------------------------------------------------------------------
+    -- Hallowfall
+    [81881] = "Hallowfall Rare: Funglour killed daily",
     [82066] = "Hallowfall: A Scout's Journal interacted",
+    
+    -- -----------------------------------------------------------------------------------------------------------------------
+    -- Azj'kahet
+    
+    -- -----------------------------------------------------------------------------------------------------------------------
+    -- Siren Isle
+    [84801] = "Siren Isle Rare: Ghostmaker killed daily",
+    [84792] = "Siren Isle Rare: Ikir the Flotsurge killed daily",
+    [85403] = "Siren Isle Rare: Tempest Talon killed daily",
+    [84798] = "Siren Isle Rare: Slaughtershell killed daily",
+    [85405] = "Siren Isle Rare: Zek'hul the shipbreaker killed daily",
+    [85404] = "Siren Isle Rare: Brinebough killed daily",
+    [84805] = "Siren Isle Rare: Asbjorn the Bloodsoaked killed daily",
+    [84794] = "Siren Isle Rare: Wreckwater killed daily",
+    [85406] = "Siren Isle Rare: Ksvir the Forgotten killed daily",
+    [86933] = "Siren Isle Rare: Snacker killed daily",
+    [84796] = "Siren Isle Rare: Grimmgull killed daily",
+    [84797] = "Siren Isle Rare: Gunnlord the Sea-Drinker killed daily",
+    [84803] = "Siren Isle Rare: Siris the Sea Scorpion killed daily",
+    [84802] = "Siren Isle Rare: Coralweaver Calliso killed daily",
+    [84795] = "Siren Isle Rare: Bloodbrine killed daily",
+    [86779] = "Siren Isle Rare: Shardsong killed daily",
+    [84799] = "Siren Isle Rare: Plank-Master Bluebelly daily",
+    [84800] = "Siren Isle Rare: Chef Chum Platter daily",
+    [85669] = "Siren Isle Excavation: Gravesludge killed in The Drain",
+    [86732] = "Siren Isle Chest: Stone Carvers Scramseax looted",
+    
+    -- -----------------------------------------------------------------------------------------------------------------------
+    -- Misc
+    [39751] = "Classic Timewalking LFG completion",
 }
